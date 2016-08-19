c78467a276e2c5cfd11793794b2d457a1cb759a6
Stephanie Gurung <stephr3@gmail.com>
2016-08-19 13:22:25 -0700

    minor change to file and readme

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   actions: {
     deleteAnswer(answer) {
       if(confirm('Are you sure you want to delete this answer?')) {
-        this.sendAction('deleteAnswer', answer)
+        this.sendAction('deleteAnswer', answer);
       }
     },
     showEditAnswer() {
