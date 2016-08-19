c94787cb583af77277f5790316b9b17af0946bae
Stephanie Gurung <stephr3@gmail.com>
2016-08-19 09:24:40 -0700

    display edit question component when pencil icon is clicked

--- app/components/question-details.js
+++ app/components/question-details.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showEditQuestion: false,
+  actions: {
+    showEditQuestion() {
+      this.set('showEditQuestion', true);
+    }
+  }
 });
