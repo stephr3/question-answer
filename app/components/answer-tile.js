d8c93781ed2da8435a3e0c2f8d7382e9e4d96c54
Stephanie Gurung <stephr3@gmail.com>
2016-08-19 11:13:12 -0700

    display edit answer form on click of pencil icon

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showEditAnswer: false,
   actions: {
     deleteAnswer(answer) {
       if(confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('deleteAnswer', answer)
       }
+    },
+    showEditAnswer() {
+      this.set('showEditAnswer', true);
     }
   }
 });
