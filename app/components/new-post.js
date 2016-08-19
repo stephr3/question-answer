28cb81e3e4715407317483005bf3ecb818365793
Stephanie Gurung <stephr3@gmail.com>
2016-08-19 09:16:00 -0700

    allow the user to save a new question to the database

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        author: this.get('author'),
+        date: this.get('date'),
+        query: this.get('query'),
+        notes: this.get('notes'),
+        tag: this.get('tag')
+      };
+      this.sendAction('save', params);
+    }
+  }
 });
